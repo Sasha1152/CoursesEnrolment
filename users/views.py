5549aeaa1bde57abf9186d460c6912349bb60e8a
Sasha1152 <saxingit config --global user.email saxin@ukr.netgit config --global user.email saxin>
2020-02-01 15:39:30 +0200

    create search user functionality

--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from . models import UserProfile
+from django.http import HttpResponse
+import json
 
 
 def get_users_list(request):
-    users = UserProfile.objects.all()
+    if request.GET:
+        name = request.GET.get('name')
+        users = UserProfile.objects.filter(name__contains=name)
+    else:
+        users = UserProfile.objects.all()
+
     return render(request, 'users.html', {'users': users})
